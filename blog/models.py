f3a7ead36e14b66c568023cb38767ebcbfd3bfb0
harsh <f20200794@pilani.bits-pilani.ac.in>
2021-11-30 22:22:00 +0530

    basic post modelling

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.utils import timezone 
+from django.contrib.auth.models import User  #to import the user models 
+
+# (class) User
+# Users within the Django authentication system are represented by this model.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    datePosted= models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    #CASCADE when a user is deleted the post also gets deleted
+    
+    #post can have only one user,whereas one user can have multiple posts  
