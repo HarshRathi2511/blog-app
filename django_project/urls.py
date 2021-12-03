9060ad568ee2b515f9464168048c2e67980343ca
harsh <f20200794@pilani.bits-pilani.ac.in>
2021-12-03 10:28:12 +0530

    login and logout functionality done

--- django_project/urls.py
+++ django_project/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import contrib
 from django.contrib import admin
 from django.urls import path,include
 from users import views as user_views
+from  django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +27,7 @@ urlpatterns = [
     # directory and then searches for url named about if it is present 
     # path('') =>for it to be the home page 
 
+    #views django gives us for logging in and logging out 
+    path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),  #class based views 
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 
 from django.contrib.auth import forms
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
@@ -9,13 +10,17 @@ from users.forms import UserRegistrationForm
 # Create your views here.
 def register(request):
     #to have the django created form
+     
     if request.method =='POST' :
         form = UserRegistrationForm(request.POST)
-        if form.is_valid() :
-          form.save() #save the user 
+        if form.is_valid() :    
+          returnedUser= form.save() #save the user
+          print('$$$$$$$$') 
+          print(returnedUser)
           username = form.cleaned_data.get('username')
-          messages.success(request,f'Account created for {username}!') #to show a snackbar 
-          return redirect('blog-home')
+          messages.success(request,f'Your account has been created ,Please login {returnedUser.username}!') #to show a snackbar 
+          # redirect them to the login page after creating the account
+          return redirect('login')
     else :
       form =UserRegistrationForm()
 
