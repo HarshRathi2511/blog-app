5b85c755fb1078025b608241cd09f867af0bbb64
harsh <f20200794@pilani.bits-pilani.ac.in>
2021-12-03 10:43:50 +0530

    logged out users cant access profile page

--- django_project/urls.py
+++ django_project/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     #views django gives us for logging in and logging out 
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),  #class based views 
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('profile/',user_views.profile,name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required 
 
 from users.forms import UserRegistrationForm
 
@@ -25,3 +26,11 @@ def register(request):
       form =UserRegistrationForm()
 
     return render(request,'users/register.html',{'form':form})
+
+@login_required   #for a logged in user to only see the profile 
+#Decorators are a very powerful and useful tool in Python
+#since it allows programmers to modify the behaviour of function or class.
+def profile(request):
+    return render(request,'users/profile.html') 
+
+ #http://127.0.0.1:8000/login/?next=/profile/  => next page redirected is the profile    
