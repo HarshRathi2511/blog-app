a20b7f33aeb94f0343a0544bde6591c4b22b6655
harsh <f20200794@pilani.bits-pilani.ac.in>
2021-12-05 18:46:18 +0530

    class based list view

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.home,name='blog-home'),  #views function mapper to the home fn which returns the http res
+    path('', views.PostListView.as_view(),name='blog-home'),  #views function mapper to the home fn which returns the http res
     path('about/', views.about,name='blog-about'),
+    path('post/<int:pk>', views.PostDetailView.as_view(),name='post-detail'), 
+    #to create the custom urls as per the blogs
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
+from typing import List
 from django.shortcuts import render  #to return a render template 
 from .models import Post 
+from django.views.generic import ListView,DetailView,DeleteView
 
-# handle the traffic from the homepage of the blog
+from blog import models
+
+# function based views 
 def home(request):   #function
     context = {
         'posts':Post.objects.all()
@@ -9,6 +13,29 @@ def home(request):   #function
     }
     return render(request,'blog/home.html',context)
 
+#class based view 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'    #the list will look here 
+    context_object_name = 'posts'   #what it will be accesed as in the frontend 
+    ordering =['-date_posted']
+
 def about(request):
-    return render(request,'blog/about.html',{'title': 'About'})    
+    return render(request,'blog/about.html',{'title': 'About'}) 
+
+class PostDetailView(DetailView):
+    model = Post
+    #template naming convention :- <app>/<model>_<viewtype>.html
+    # template_name = 'blog/home.html'    #the list will look here 
+    # context_object_name = 'posts'   #what it will be accesed as in the frontend 
+
+   
+
+# Class based views 
+# List view eg- List of videos in utube
+# Detail View 
+# Class view
+# Update view  
+# Delete view 
+  
 
